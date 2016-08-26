9f4d46ad58191dc091191ca6ecff915961e92e55
Vitor Prado <jvirtuprado@gmail.com>
2016-08-26 15:28:07 -0300

    Make project build for swift 3

--- PKHUD/WindowRootViewController.swift
+++ PKHUD/WindowRootViewController.swift
@@ -12,35 +12,46 @@ import UIKit
 /// Serves as a configuration relay controller, tapping into the main window's rootViewController settings.
 internal class WindowRootViewController: UIViewController {
     
-    internal override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if let rootViewController = UIApplication.sharedApplication().delegate?.window??.rootViewController {
-            return rootViewController.supportedInterfaceOrientations()
-        } else {
-            return UIInterfaceOrientationMask.Portrait
+    open override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        get {
+            if let rootViewController = UIApplication.shared.delegate?.window??.rootViewController {
+                return rootViewController.supportedInterfaceOrientations
+            } else {
+                return UIInterfaceOrientationMask.portrait
+            }
         }
     }
     
-    internal override func preferredStatusBarStyle() -> UIStatusBarStyle {
-        return self.presentingViewController?.preferredStatusBarStyle() ?? UIApplication.sharedApplication().statusBarStyle
-    }
-    
-    internal override func prefersStatusBarHidden() -> Bool {
-        return self.presentingViewController?.prefersStatusBarHidden() ?? UIApplication.sharedApplication().statusBarHidden
-    }
-    
-    internal override func preferredStatusBarUpdateAnimation() -> UIStatusBarAnimation {
-        if let rootViewController = UIApplication.sharedApplication().delegate?.window??.rootViewController {
-            return rootViewController.preferredStatusBarUpdateAnimation()
-        } else {
-            return .None
+    open override var preferredStatusBarStyle: UIStatusBarStyle {
+        get {
+            return self.presentingViewController?.preferredStatusBarStyle ?? UIApplication.shared.statusBarStyle
         }
     }
     
-    internal override func shouldAutorotate() -> Bool {
-        if let rootViewController = UIApplication.sharedApplication().delegate?.window??.rootViewController {
-            return rootViewController.shouldAutorotate()
-        } else {
-            return false
+    open override var prefersStatusBarHidden: Bool {
+        get {
+            return self.presentingViewController?.prefersStatusBarHidden ??
+                UIApplication.shared.isStatusBarHidden
+        }
+    }
+    
+    open override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
+        get {
+            if let rootViewController = UIApplication.shared.delegate?.window??.rootViewController {
+                return rootViewController.preferredStatusBarUpdateAnimation
+            } else {
+                return .none
+            }
+        }
+    }
+    
+    open override var shouldAutorotate: Bool {
+        get {
+            if let rootViewController = UIApplication.shared.delegate?.window??.rootViewController {
+                return rootViewController.shouldAutorotate
+            } else {
+                return false
+            }
         }
     }
 }
